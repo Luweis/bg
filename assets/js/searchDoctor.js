c5761a12619248974b44265d99e589dfb6e1fd8f
luwei <lu.w@ikcrm.com>
2018-04-13 22:34:48 +0800

    完成医生搜索页面

--- assets/js/searchDoctor.js
+++ assets/js/searchDoctor.js
@@ -12,24 +12,33 @@ var doctorTypeEnum = {
 
 const block = $(function () {
     const params = getParams(location.search);
+    if (params.page === undefined){
+      params.page = 1;
+  }
     const currentPage = params.page || 1;
     const pre = $('.searchDoctor-pagination-button-prev');
     const next = $('.searchDoctor-pagination-button-next');
     const total = Number($('#search-total-value').text());
     const totalPage = Math.ceil(total / doctorTypeEnum.perPage);
-    if (currentPage===1) {
+    if (currentPage == 1) {
         pre.hide();
-    }else if (currentPage === totalPage){
+    }else if (currentPage == totalPage){
         next.hide();
     }
 
     pre.on('click', function () {
-        location.href = ``
+      params.page = Number(params.page) - 1;
+      window.location.href = `./search?${getQueryParams(params)}`;
+    });
+    next.on('click', () =>{
+      params.page = Number(params.page) + 1;
+      window.location.href = `./search?${getQueryParams(params)}`;
     })
 });
 
 function getParams(search) {
  search = search.replace(/\?/, '').replace(/&&/g,'&');
+ search = decodeURIComponent(search);
  const params = {};
  const array = search.split('&') || [];
  array.map((item) =>{
@@ -39,16 +48,9 @@ function getParams(search) {
  return params;
 }
 
-// function initPagination (total) {
-//     var totalPage = Math.ceil(total/10);
-//      const pre = $('.searchDoctor-pagination-button-prev');
-//      const next = $('.searchDoctor-pagination-button-next');;
-//      if (totalPage<2) {
-//          next.hide();
-//      }else if(totalPage 10) {
-//
-//      }
-// }
+function getQueryParams(obj) {
+ return Object.keys(obj).map((key) => `${key}=${obj[key]}`).join('&');
+}
 
 export default block;
 
