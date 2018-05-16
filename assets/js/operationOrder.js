10c3ff3667efc42099d91fd0e31d4e9f2771250b
luwei <lu.w@ikcrm.com>
2018-05-16 13:26:42 +0800

    修复bug

--- assets/js/operationOrder.js
+++ assets/js/operationOrder.js
@@ -9,20 +9,14 @@ $('.operationOrder-search-button').on('click', function () {
 
 // yy-bg-ques-btn
 $('.yy-bg-ques-btn').on('click', function (e) {
-  console.log('yyyyyyyyy')
-  showModal();
-  var dcotor = $(e.target).siblings('.yy-banner-card-h3').text();
+
+  var doctorName = $(e.target).siblings('.yy-banner-card-h3').text();
+  var hospitalName = $(e.target).siblings('.yy-banner-card-short').last().text();
   var depName = $(e.target).siblings('.yy-banner-card-short').find('span').text();
-  var hospitalName = $(e.target).siblings('.yy-banner-card-short').last().text()
-  $('#doctorName').val(dcotor);
-  $('#hospitalName').val(hospitalName)
-  $('#depName').val(depName)
-  e.preventDefault();
+  showModal(doctorName, hospitalName, depName, e);
 });
 
 $('.home-yy-shoushu').on('click', function (e) {
-  console.log('yyyyyyyyy')
-  showModal()
   var $paraent = $(e.target).parent('.bg-ques-card-head')
     .siblings('.bg-ques-card-content')
   var doctorName = $paraent
@@ -30,24 +24,29 @@ $('.home-yy-shoushu').on('click', function (e) {
     .text().trim();
   var hospitalName = $paraent.find('.hospitalName').text().trim();
   var depName = $paraent.find('.departmentName').text().trim();
-  $('#doctorName').val(doctorName);
-  $('#hospitalName').val(hospitalName);
-  $('#depName').val(depName);
+  showModal(doctorName, hospitalName, depName, e);
   e.preventDefault();
 });
 
-$('#ques-doctor-btn').on('click',function () {
+$('#ques-doctor-btn').on('click', function () {
   $('#queModal').modal().show();
 
 });
 
-$('.home-page-detail').on('click',function () {
+$('.home-page-detail').on('click', function () {
   $('#operationModal').modal().show();
+})
+
+$("#doctor-home-detail").on('click', function (e) {
+  var doctorName = $('#docName').text().trim();
+  var hospitalName = $('#doc_hospitalName').text().trim();
+  var depName = $('#doc_departmentName').text().trim();
+    showModal(doctorName, hospitalName, depName, e);
 })
 
 function showModal(realName, department, hospitalName, e) {
   var evt = e ? e : window.event;
-  evt.stopPropagation();
+  evt.preventDefault();
   $('#doctorName').val(realName);
   $('#depName').val(department);
   $('#hospitalName').val(hospitalName);
